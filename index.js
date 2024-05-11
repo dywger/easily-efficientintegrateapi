410e54668152a4a7231b240b09db600f907f25a9
dywger <esjcmirdikmen02@hotmail.com>
2024-05-12 00:10:32 +0700

    test

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("world");
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+  return false;
+}
