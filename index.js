fb7f3167b438bf06b1e3adc90e3ce4b1f636f40c
dywger <esjcmirdikmen02@hotmail.com>
2024-05-14 08:04:22 +0700

    feat task

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-// Fund Alice and sponsor accounts
-await aptos.fundAccount({
-  accountAddress: aliceAddress,
-  amount: ALICE_INITIAL_BALANCE,
-});
-await aptos.fundAccount({
-  accountAddress: sponsorAddress,
-  amount: SPONSOR_INITIAL_BALANCE,
-});
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
 
-// Show account balances
-const aliceBalanceBefore = await aptos.getAccountCoinsData({
-  accountAddress: aliceAddress,
-});
-const sponsorBalanceBefore = await aptos.getAccountCoinsData({
-  accountAddress: sponsorAddress,
-});
+greet("Bob");
